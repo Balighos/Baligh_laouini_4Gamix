8b5b362100eff4b52207d329b4aaf38aa40da0f5
BaliActual <Baligh.laouini@esprit.tn>
2025-10-10 13:24:45 +0100

    first commit 3eme seance

---
+++ src/main/java/tn/esprit/baligh_laouini_4gamix1/BalighLaouini4Gamix1Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.baligh_laouini_4gamix1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalighLaouini4Gamix1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalighLaouini4Gamix1Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/baligh_laouini_4gamix1/BalighLaouini4Gamix1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.baligh_laouini_4gamix1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalighLaouini4Gamix1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
